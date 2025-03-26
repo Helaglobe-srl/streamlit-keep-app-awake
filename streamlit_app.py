528a3f486aa9ca14b6cf492e22366f6a97d60af5
Enrico Nello <55366018+enricollen@users.noreply.github.com>
2025-03-26 10:12:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # List of Streamlit App URLs
 
 STREAMLIT_APPS = [
-    "https://tema2024.streamlit.app/",
-    "https://patientengagementaward2025.streamlit.app/"
+    "https://patientengagementaward2025.streamlit.app/",
+    "https://tema2024.streamlit.app/"
 ]
